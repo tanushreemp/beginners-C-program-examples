b2e9e9b1f80a2b6bc032402fcc9fd5fe4cc4acb3
sriram gn <38837878+Sriramgn0@users.noreply.github.com>
2019-10-29 18:33:33 +0530

    Update Prime.c
    time complexity decreased

--- Prime.c
+++ Prime.c
@@ -10,19 +10,8 @@ void main(){
     //traversing all numbers from 2 to entered integer num
     for(i=2; i<num; i++)
     {
-        //loop to check divisibility
-        for(j=2; j<i; j++)
+        if((i == 2)||(i==3)||((i+1)%6==0)||((i-1)%6==0))
         {
-            //checking if it is divisible by any smaller number
-            check=sqrt(i);
-            if(check%j == 0)
-            {
-                //if it is divisible exiting from loop
-                break;
-            }
-        }
-        //printing the number if it is never divisible by numbers smaller than itself
-        if(i == j){
             printf("%d\n", i);
         }
     }
