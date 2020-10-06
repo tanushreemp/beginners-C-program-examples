8e2327d3cc5ad55a6acb0616001f67e1b5ab3b2b
tanushreemp <57219827+tanushreemp@users.noreply.github.com>
2020-10-06 10:44:21 +0530

    Update area of triangle
    Calucalting the area of triangle  by using c program,

--- Area_of_Triangle.c
+++ Area_of_Triangle.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include<stdio.h>//header file 
 void main()
 { float a,b,h;
-printf("Enter the base : ");
-scanf("%f",&b);
-printf("Enter the height : ");
+printf("Enter the base =: ");
+scanf("%f",&b);reading the value 
+printf("Enter the height =: ");
 scanf("%f",&h);
-a=0.5*b*h;
-printf("Area is %f",a);
+a=0.5*b*h;//formula of area 
+printf("Area is %f",a);//printing the value 
 }
